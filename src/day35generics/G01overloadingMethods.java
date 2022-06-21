1e9dac34f8576cecb7c182cbeebb7aa18f4c5f47
fersatir <fatihersatir@gmail.com>
2022-06-21 18:32:23 +0300

    v11

--- src/day35generics/G01overloadingMethods.java
+++ src/day35generics/G01overloadingMethods.java
@@ -1,10 +1,35 @@
 package day35generics;
 
+import java.util.Arrays;
+
 public class G01overloadingMethods {
     public static void main(String[] args) {
-        Integer [] intDizi = {1,2,3,4,5,6,7,8};
-        Double [] doubleDizi = {1.1,2.2,3.3,4.4,5.5};
-        Character [] charDizi = {'T','U','B','A'};
-        System.out.println("Integer Dizi");
+        Integer[] intDizi = {1, 2, 3, 4, 5, 6, 7};
+        Double[] doubleDizi = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
+        Character[] charDizi = {'T', 'U', 'G', 'B', 'A' };
+        System.out.println("integer dizi: ");
+        diziYazdir(intDizi);
+        System.out.println("double dizi: ");
+        diziYazdir(doubleDizi);
+        System.out.println("char dizi: ");
+        diziYazdir(charDizi);
+        System.out.println("***  Super Object Class *** ");
+        diziYazdirObject(intDizi);
+        diziYazdirObject(doubleDizi);
+        diziYazdirObject(charDizi);
     }
+    //Overloaded methods...
+    public static void diziYazdir(Integer[] dizi) {//parametre için wraper class sartı yok
+        Arrays.stream(dizi).forEach(System.out::println);
+    }
+    public static void diziYazdir(Double[] dizi) {
+        Arrays.stream(dizi).forEach(System.out::println);
+    }
+    public static void diziYazdir(Character[] dizi) {
+        Arrays.stream(dizi).forEach(System.out::println);
+    }
+    public static void diziYazdirObject(Object[] dizi) {
+        Arrays.stream(dizi).forEach(System.out::println);
+    }
+    //Object Class'daki datalarin type'ı farklı oldg için bu veriler alınırken casting yapılmazsa CTE verir
 }
