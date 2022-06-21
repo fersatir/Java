1b356075eac3bc4f36bbd8eed46a78d6e79bfe4b
fersatir <fatihersatir@gmail.com>
2022-06-21 18:24:00 +0300

    v9

---
+++ src/day35generics/G01overloadingMethods.java
@@ -0,0 +1,9 @@
+package day35generics;
+
+public class G01overloadingMethods {
+    public static void main(String[] args) {
+        Integer [] intDizi = {1,2,3,4,5,6,7,8};
+        Double [] doubleDizi = {1.1,2.2,3.3,4.4,5.5};
+        Character [] charDizi = {'T','U','B','A'};
+    }
+}
