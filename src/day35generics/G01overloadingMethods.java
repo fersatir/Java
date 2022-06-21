d81d82c3b28306e34ced5afcece6f1fab7ce4cdb
fersatir <fatihersatir@gmail.com>
2022-06-21 18:26:53 +0300

    v10

--- src/day35generics/G01overloadingMethods.java
+++ src/day35generics/G01overloadingMethods.java
@@ -5,5 +5,6 @@ public class G01overloadingMethods {
         Integer [] intDizi = {1,2,3,4,5,6,7,8};
         Double [] doubleDizi = {1.1,2.2,3.3,4.4,5.5};
         Character [] charDizi = {'T','U','B','A'};
+        System.out.println("Integer Dizi");
     }
 }
